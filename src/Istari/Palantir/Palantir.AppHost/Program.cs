559a76aa77b8757ea053274d06c36874dd50157b
Andrei Maslov <autumnusmors@yandex.ru>
2025-08-30 13:45:52 +0400

    Sauron db migrator

--- src/Istari/Palantir/Palantir.AppHost/Program.cs
+++ src/Istari/Palantir/Palantir.AppHost/Program.cs
@@ -5,8 +5,14 @@ var postgreServer = builder
 	.WithPgAdmin();
 var sauronDb = postgreServer.AddDatabase("saurondb");
 
+var sauronDbMigrator = builder
+	.AddProject<Projects.Sauron_DbMigrator>("saurondbmigrator")
+	.WithReference(sauronDb, "Sauron")
+	.WaitFor(sauronDb);
+
 var sauron = builder
 	.AddProject<Projects.Sauron_IdentityWebApp>("sauron")
-	.WithReference(sauronDb, "Sauron");
+	.WithReference(sauronDb, "Sauron")
+	.WaitFor(sauronDb);
 
 builder.Build().Run();
---
+++ src/Istari/Sauron/Sauron.DbMigrator/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Sauron.Domain.EfCore;
+using Sauron.Domain.EfCore.DependencyInjection;
+
+var host = Host.CreateApplicationBuilder(args);
+host.Services.AddEfCore(host.Configuration);
+var application = host.Build();
+
+await using (var scope = application.Services.CreateAsyncScope())
+{
+	var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+	
+	logger.LogInformation("Run migrations");
+	var sauronContext = scope.ServiceProvider.GetRequiredService<SauronDbContext>();
+	await sauronContext.Database.MigrateAsync();
+	logger.LogInformation("Migrations end");
+}
+
